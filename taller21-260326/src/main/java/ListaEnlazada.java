cf2c5e12b0b776897a249d8ffcd2acd5cbce21c1
Andres Felipe <est.andresf.garciac@unimilitar.edu.co>
2026-03-26 10:26:21 -0500

    error presente en clase lista enlazada 5ta linea correccion (=)- (==)

--- taller21-260326/src/main/java/ListaEnlazada.java
+++ taller21-260326/src/main/java/ListaEnlazada.java
@@ -1,6 +1,24 @@
 public class ListaEnlazada {
     Nodo cabeza;
     public void insertar(int valor){
-        Nodo nuevo = new Nodo(valor)
+        Nodo nuevo = new Nodo(valor);
+        if (cabeza = null){
+            cabeza = nuevo;    
+        }else{
+            Nodo temp = cabeza;
+            while (temp.siguiente != null ){
+                temp= temp.siguiente;
+            }
+            temp.siguiente = nuevo;
+        }
+    }
+    public int suma (){
+        int total= 0;
+        Nodo temp = cabeza;
+        while (temp != null){
+            total = total + temp.valor;
+            temp = temp.siguiente; 
+        }
+        return total;
     }
 }
