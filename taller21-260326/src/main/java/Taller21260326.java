a0686492ec1adae99efe05abd50b8a33fa11a505
Andres Felipe <est.andresf.garciac@unimilitar.edu.co>
2026-03-26 10:56:30 -0500

    el programa se ejecuta correctamente y por consola se ve ordenado

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -6,10 +6,12 @@ public class Taller21260326 {
     public static void main(String[] args) {
         
         Scanner sc= new Scanner(System.in);
+        System.out.println("Digite la cantidad total de enteros (n)");
         int n = sc.nextInt();
         
         ListaEnlazada Lista = new ListaEnlazada();
         
+        System.out.println("Ahora digite " + (n-1) + "numeros separados por espacio o enter: ");
         for(int i = 0 ; i < n-1 ; i++){
          
             int num = sc.nextInt();
